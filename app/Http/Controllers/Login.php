433eb7d23e575880afe933108cb05682f9dac553
mansour13w <mansourboumediene.pro@gmail.com>
2023-04-24 13:01:46 +0200

    push2

--- app/Http/Controllers/Login.php
+++ app/Http/Controllers/Login.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
-class Connexion extends Controller
+class Login extends Controller
 {
     public function login(){
      
